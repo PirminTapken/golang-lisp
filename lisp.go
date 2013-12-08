34c6cc231be1f2e5608f46d9eba7ea496af7d6e9
Pirmin Fix <pirmin.fix@jimdo.com>
2013-12-08 20:50:37 +0100

    Making tests green again

--- lisp.go
+++ lisp.go
@@ -2,12 +2,27 @@ package lisp
 
 import (
 	"container/list"
+	"strings"
+	"text/scanner"
 )
 
 func Tokenize(s string) *list.List {
 	l := new(list.List)
-	l.PushBack("a")
-	l.PushBack("b")
-	l.PushBack("c")
+
+	r := strings.NewReader(s)
+
+	var (
+		sc scanner.Scanner
+		ts string
+	)
+
+	sc.Init(r)
+	for tok := sc.Scan(); tok != scanner.EOF; tok = sc.Scan() {
+		ts = sc.TokenText()
+		if ts != "(" && ts != ")" {
+			l.PushBack(ts)
+		}
+	}
+
 	return l
 }
